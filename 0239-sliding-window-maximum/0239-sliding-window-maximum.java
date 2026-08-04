e16ee313413c44788443b3ed98dd489f38768d91
Gauri_Chauhan <cgauri915@gmail.com>
2026-08-04 14:23:48 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int[] maxSlidingWindow(int[] nums, int k) {
+         int n = nums.length;
+        int[] res = new int[n-k+1]; //extra nhi bnana size
+
+        for (int i=0; i<=n-k; i++) {
+            int max = nums[i];
+            for (int j =i; j<i+k; j++) {    //k tk ki hi window size lena  hhh
+                max = Math.max(max, nums[j]);  
+            }
+            res[i] = max;
+        }
+        return res;
+    }
+}
